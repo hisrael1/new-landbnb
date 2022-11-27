5daffeefc7359114016521f6397ff0a5d68a29ff
hisrael1 <harrisonisrael23@gmail.com>
2022-11-27 16:02:52 -0500

    user backend working

---
+++ app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ApplicationRecord
+    validates :email, :first_name, :last_name, :session_token, :password_digest, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+
+    after_initialize :ensure_session_token
+
+    attr_reader :password
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+
+        if user && user.is_password?(password)
+            return user
+        else
+            nil
+        end
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def self.generate_session_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+    
+    def reset_session_token!
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
+    end
+end
