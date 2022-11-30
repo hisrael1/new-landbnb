7acb28305be20d3ff6c2fd09502266cbaf284b9f
hisrael1 <harrisonisrael23@gmail.com>
2022-11-30 01:02:03 -0500

    ShowListing

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -7,7 +7,7 @@ import Splash from "./splash/Splash";
 import ListingsContainer from "./listings/ListingsContainer";
 import UserContainer from "./user/UserContainer";
 import NewListingContainer from "./listings/NewListingContainer";
-// import ShowListingContainer from "./listings/ShowListingContainer";
+import ShowListingContainer from "./listings/ShowListingContainer";
 // import CreateReviewContainer from "./reviews/CreateReviewContainer";
 // import Trips from "./booking/Trips";
 
@@ -20,9 +20,9 @@ const App = () => (
         <Route path = "/listings/:query" component={ListingsContainer} />
         <Route path = "/users/show/" component={UserContainer}/>
         <Route path = "/listing/new/" exact component={NewListingContainer}/>
-        {/* <Route path = "/listing/:id/review/new/" component={CreateReviewContainer}/> 
+        {/* <Route path = "/listing/:id/review/new/" component={CreateReviewContainer}/>  */}
         <Route path = "/listing/:id/" component={ShowListingContainer}/>
-        <Route path = "/user/:id/bookings" component={Trips}/> */}
+        {/* <Route path = "/user/:id/bookings" component={Trips}/> */}
                
     </Switch>
 );
